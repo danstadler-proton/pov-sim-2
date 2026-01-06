05d0d910bbb2485e12446422f257e0a9077df728
danstadler-proton <danstadler@proton.me>
2026-01-06 01:29:25 +0000

    Faro changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,26 @@ import Airlines from './pages/Airlines';
 import Flights from './pages/Flights';
 import Navigation from './components/Navigation';
 
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-us-central-0.grafana.net/collect/664ce2779d86373b665e06edb9b65f44',
+  app: {
+    name: 'december-2025-pov-sim-testing',
+    version: '1.0.0',
+    environment: 'production'
+  },
+  
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+  ],
+});
+
 function App() {
   return (
     <div className="App">
